51702ec301f7a15e2a9035fc003267463dc0f9e3
nmcsakib <nmcsakib.1@gmail.com>
2025-05-18 17:09:58 +0600

    created ph-tube cards

--- js/script.js
+++ js/script.js
@@ -24,21 +24,24 @@ const loadVideos = async () => {
     const videos = data.videos;
     console.log(videos);
     videos.forEach(video => {
-        console.log(video);
+        console.log(video.thumbnail);
         const videoCard = document.createElement('div');
         videoCard.classList = 'card shadow-sm';
         videoCard.innerHTML = `
-        <figure>
-    <img
+        <figure class='h-[200px]'>
+    <img class="w-full object-cover h-full cursor-pointer"
       src="${video.thumbnail}"
       alt="Shoes" />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
+  <div class="flex px-2 py-4 gap-4">
+  <img class="w-10 h-10 rounded-full" src="${video.authors[0].profile_picture}" alt="" />
+    <div class="flex flex-col">
+         <h2 class="card-title">${video.title}</h2>
+        <p class="text-sm text-gray-500 font-bold py-1">${video.authors[0].profile_name}
+        <img width="16" height="16" class='${video.authors[0].verified ? 'inline' : 'hidden'}' src="https://img.icons8.com/fluency-systems-filled/48/228BE6/verified-badge.png" alt="verified-badge"/>
+        </p>
+        <p class="text-sm text-gray-500 font-semibold">${video.others.views} views</p>
+        </div>
   </div>`;
         videoContainer.appendChild(videoCard);
     })
